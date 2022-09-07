819f63105c12ddb07e4be4d7b5a2e67aaf11adab
Júlia Teixeira <julia_vlj@hotmail.com>
2022-09-06 21:43:05 -0300

    Segundo exerc incompleto

--- prova.js
+++ prova.js
@@ -1,18 +1,29 @@
-let numero = parseInt(prompt("Insira um número: "))
+// let numero = parseInt(prompt("Insira um número: "))
 
-console.log(RetornarValor(numeroParametro))
-function RetornarValor(numeroParametro) {
-    if (numeroParametro == 2) {
-        return true
-    } else {
-        return false
-    }
-}
-// 2. Escreva uma função que recebe, por parâmetro, um valor inteiro e positivo e retorna
-// o somatório de 1 até esse valor. Ex.: Entrada = 5 a função tem que retornar o total
-// de 1 + 2 + 3 + 4 + 5
+// console.log(RetornarValor(numeroParametro))
+// function RetornarValor(numeroParametro) {
+//     if (numeroParametro == 2) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// let num = parseInt(prompt("Insira um número: "))
+// let array = [1]
+
+// RetornarSoma(numParametro)
+
+// console.log( )
+
+// function RetornarSoma(numParametro){
+// for (let index = 1; index < numParametro; index++) {
+//     numParametro = [array] 
+    
+// }
+// }
 // 3. Faça uma função que receba dois valores como parâmetro A e B. Se A for par e B
 // maior que 5 retorne o valor lógico “true” senão retorna “false”
+
 // 4. Faça uma função que recebe 3 valores inteiros por parâmetro e retorna-os em um
 // array ordenado em ordem crescente.
 // 5. Crie um array com 10 elementos. Faça uma função que recebe dois valores como
