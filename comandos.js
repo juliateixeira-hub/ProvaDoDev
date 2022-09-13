27591e680d0f6e1e0e2c293fb4c6661df5a61a26
Júlia Teixeira <julia_vlj@hotmail.com>
2022-09-12 21:07:45 -0300

    Comandos de javascript

---
+++ comandos.js
@@ -0,0 +1,12 @@
+let nome = "Júlia"
+
+// console.info(PerguntarNome(nome)) // .info mesmo comportamento que .log
+// function PerguntarNome(nome)
+
+// console.log(prompt("mostra um erro aí")) // prompt() pede que o usuário insira algo
+
+// console.error() // apresenta um arro 
+
+// window.alert("fala comigo") // apresenta um alerta na tela 
+
+//console.warn("coé") // .warn imprime algo parecido com o .alert
